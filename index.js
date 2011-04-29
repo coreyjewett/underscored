9d9fb2d1838d08f6ec070730c561262250f093b8
Corey Jewett <cj@syntheticplayground.com>
2011-04-29 11:41:57 -0700

    Cleaning up Packaging. NPM loads underscore.string.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var _ = require('./lib/underscore/underscore');
-
-_.mixin(require('./lib/underscore.string/lib/underscore.string'));
+_.mixin(require('underscore.string'));
 _.mixin(require('./lib/custom'))
 
 module.exports = _;
